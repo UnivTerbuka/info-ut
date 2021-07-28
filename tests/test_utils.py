afe06747a83b4f54b1a81c3e96c40523dae154d1
hexatester <revolusi147id@gmail.com>
2021-07-28 17:33:00 +0700

    Added test_get_feed

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,11 @@ from info_ut.feed import Feed
 from info_ut.utils import get_feed, get_berita, get_pengumuman
 
 
+def test_get_feed():
+    feed = get_feed("https://www.ut.ac.id/pengumuman.xml")
+    assert isinstance(feed, Feed)
+
+
 def test_get_berita():
     feed = get_berita()
     assert isinstance(feed, Feed)
