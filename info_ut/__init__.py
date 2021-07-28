7af06b04dff2a31fd733496d4d300ffe757b0ae3
hexatester <revolusi147id@gmail.com>
2021-07-28 20:09:40 +0700

    Bumping version from 0.1.0 to 0.1.1

--- info_ut/__init__.py
+++ info_ut/__init__.py
@@ -2,6 +2,6 @@ from .feed import Feed
 from .post import Post
 from .utils import get_berita, get_pengumuman
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["Feed", "get_berita", "get_pengumuman", "Post"]
